f3b7e8e74327a6971ad1b006d37b985c3eff1534
msalehprofile <mollyhannia@hotmail.co.uk>
2024-07-10 17:19:04 +0100

    have user table, can check if email exists and get user from email

---
+++ src/main/java/com/spendingTracker/SpendingTracker/SpendingTrackerController.java
@@ -0,0 +1,43 @@
+package com.spendingTracker.SpendingTracker;
+
+import com.spendingTracker.SpendingTracker.DatabaseTables.SpendingTracking;
+import com.spendingTracker.SpendingTracker.DatabaseTables.Users;
+import com.spendingTracker.SpendingTracker.Interfaces.SpendingTrackingRepository;
+import com.spendingTracker.SpendingTracker.Interfaces.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class SpendingTrackerController {
+
+  @Autowired
+  SpendingTrackerService spendingTrackerService;
+
+  @PostMapping("createuser")
+  public Users addUserToDataBase(@RequestBody Users user) {
+    spendingTrackerService.addUserToDatabase(user);
+    return user;
+  }
+
+  @PostMapping("/addspend")
+  public SpendingTracking addSpendToDatabase(@RequestBody SpendingTracking spendingTracking) {
+    spendingTrackerService.addSpendToDatabase(spendingTracking);
+    return spendingTracking;
+  }
+
+  @GetMapping("/findUser/{email}")
+  public ResponseEntity<Boolean> findIfEmailExists(@PathVariable String email) {
+    return ResponseEntity.status(HttpStatus.OK).body(spendingTrackerService.checksEmailAlreadyExists(email));
+  }
+
+  @GetMapping("/findUserByEmail/{email}")
+  public  ResponseEntity<Users> findUserByEmail(@PathVariable String email) {
+    return ResponseEntity.status(HttpStatus.OK).body(spendingTrackerService.findUserByEmail(email));
+  }
+
+
+}
